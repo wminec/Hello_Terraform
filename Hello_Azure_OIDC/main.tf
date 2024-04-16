07ad0250be3a1f2778a727ba4884b9afbce5455f
wminec <wminec@gmail.com>
2024-04-16 22:51:33 +0900

    add Azure OIDC sample

---
+++ Hello_Azure_OIDC/main.tf
@@ -0,0 +1,19 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+resource "random_pet" "prefix" {}
+
+provider "azurerm" {
+  features {}
+
+  use_oidc  = true
+}
+
+resource "azurerm_resource_group" "default" {
+  name     = "${random_pet.prefix.id}-rg"
+  location = "West US 2"
+
+  tags = {
+    environment = "Demo"
+  }
+}
---
+++ Hello_Azure_OIDC/variables.tf
@@ -0,0 +1,14 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+variable "subscription_id" {
+  description = "Azure subscription id"
+}
+
+variable "client_id" {
+  description = "Azure Entra Application(Client) id"
+}
+
+variable "tanent_id" {
+  description = "Azure Entra Directory(tenant) id"
+}
